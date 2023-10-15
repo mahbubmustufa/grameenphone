035c2f0e63b3ea1821aecea18ea06df4e53caa51
mahbubmustufa <mahbubmustufa@gmail.com>
2023-10-15 17:02:19 +0600

    setup done

---
+++ grameenphone/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2 className='text-4xl text-bold text-center'>hello</h2>
+    </>
+  )
+}
+
+export default App
